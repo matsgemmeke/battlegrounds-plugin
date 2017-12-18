8e824020f97d698f4a5b99cfbc05e74d3022e5de
Mats Gemmeke <matsgemmeke@gmail.com>
2017-12-18 11:04:28 +0100

    Initial commit

---
+++ src/main/java/com/matsg/battlegrounds/BattlegroundsPlugin.java
@@ -0,0 +1,11 @@
+package com.matsg.battlegrounds;
+
+import com.matsg.battlegrounds.api.Battlegrounds;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BattlegroundsPlugin extends JavaPlugin implements Battlegrounds {
+
+    public void onEnable() {
+        System.out.println("Test");
+    }
+}
---
+++ src/main/java/com/matsg/battlegrounds/api/Battlegrounds.java
@@ -0,0 +1,5 @@
+package com.matsg.battlegrounds.api;
+
+import org.bukkit.plugin.Plugin;
+
+public interface Battlegrounds extends Plugin { }
